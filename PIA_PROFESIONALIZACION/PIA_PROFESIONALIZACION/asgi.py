8e0dea29b8517c1d263d9305891ef393d6aebb2e
CarlosAvila31 <carrosavila@gmail.com>
2026-04-20 23:59:59 -0600

    Ahora la pagina usa DJANGO

---
+++ PIA_PROFESIONALIZACION/PIA_PROFESIONALIZACION/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PIA_PROFESIONALIZACION project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PIA_PROFESIONALIZACION.settings')
+
+application = get_asgi_application()
