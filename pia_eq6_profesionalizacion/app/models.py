7ac6d9f8af5688e8233e702396113ebefb381e98
Devany Garza <arleth.garza023@gmail.com>
2026-04-28 19:14:52 -0600

    Base definida

--- pia_eq6_profesionalizacion/app/models.py
+++ pia_eq6_profesionalizacion/app/models.py
@@ -8,3 +8,17 @@ class Servicio(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Venta(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)  
