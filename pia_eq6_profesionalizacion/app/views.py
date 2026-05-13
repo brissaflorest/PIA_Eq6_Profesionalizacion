b7310e3415bb3f3d124214b5d277dd40d7bb0e94
Devany Garza <arleth.garza023@gmail.com>
2026-05-12 19:00:50 -0600

    Fecha del ticket arreglada

--- pia_eq6_profesionalizacion/app/views.py
+++ pia_eq6_profesionalizacion/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Servicio, Cliente, Venta, DetalleVenta
+from django.utils import timezone
 import json
 
 def servicio_list(request):
@@ -25,40 +26,15 @@ def registrar_venta(request):
         venta = Venta.objects.create(cliente=cliente, total=servicio.precio)
         DetalleVenta.objects.create(venta=venta, servicio=servicio)
 
-        return JsonResponse({
-            'folio': venta.id,
-            'cliente': cliente.nombre,
-            'plan': servicio.nombre,
-            'total': str(venta.total),
-            'fecha': venta.fecha.strftime('%d/%m/%Y %H:%M'),
-        })
-# Create your views here.
-def registrar_venta(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        nombre = data.get('nombre')
-        plan_nombre = data.get('plan')
-        precio = data.get('precio')
-
-        # Buscar o crear el servicio
-        servicio, _ = Servicio.objects.get_or_create(
-            nombre=plan_nombre,
-            defaults={'descripcion': plan_nombre, 'precio': precio}
-        )
-
-        # Crear cliente y venta
-        cliente = Cliente.objects.create(nombre=nombre)
-        venta = Venta.objects.create(cliente=cliente, total=servicio.precio)
-        DetalleVenta.objects.create(venta=venta, servicio=servicio)
+        fecha_local = timezone.localtime(venta.fecha)
 
         return JsonResponse({
             'folio': venta.id,
             'cliente': cliente.nombre,
             'plan': servicio.nombre,
             'total': str(venta.total),
-            'fecha': venta.fecha.strftime('%d/%m/%Y %H:%M'),
+            'fecha': fecha_local.strftime('%d/%m/%Y %H:%M'),
         })
-    
 
 def listado_ventas(request):
     ventas = Venta.objects.all()  
